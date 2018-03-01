729845fe7f29a20b0c3adc48c6852b48f5a67023
Sandeep Maurya <samau@microsoft.com>
2018-03-01 07:21:55 +0530

    - Adding some dropdown cells.

--- ClientApp/components/GridExample2.js
+++ ClientApp/components/GridExample2.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Grid, GridColumn as Column, GridDetailRow } from '@progress/kendo-react-grid';
+import { Grid, GridColumn as Column, GridDetailRow, GridCell, DropDownList } from '@progress/kendo-react-grid';
+
+class MyCommandCell extends GridCell {
+    render() {
+        return (
+            <td>
+                <button
+                    className="k-primary k-button k-grid-edit-command"
+                > {JSON.stringify(this.props.dataItem.description)}
+                </button>
+            </td>
+        );
+    }
+};
+
+class MyDropDownCell extends GridCell {
+    sizes = ["X-Small", "Small", "Medium", "Large", "X-Large", "2X-Large"];
+    render() {
+        return (
+            <td>
+                <select>
+                    {this.sizes.map(function (name, index) {
+                        return <option value={index}>{name}</option>;
+                    })}
+                </select>
+            </td>
+        );
+    }
+};
 
 class DetailComponent extends GridDetailRow {
+    constructor(props) {
+        super(props);
+        this.CommandCell = MyCommandCell;
+        this.DropDownCell = MyDropDownCell;
+    }
+
     render() {
         return (
             <Grid data={this.props.dataItem.details}>
                 <Column field="productId" title="Product ID" width="120px" />
                 <Column field="productName" title="Product Name" />
                 <Column field="unitPrice" title="Unit Price" format="{0:c}" />
+                <Column cell={this.DropDownCell} />
+                <Column cell={this.DropDownCell} />
+                <Column cell={this.DropDownCell} />
+                <Column cell={this.DropDownCell} />
+                <Column cell={this.DropDownCell} />
+                <Column cell={this.DropDownCell} />
             </Grid>
         );
     }
@@ -24,6 +64,8 @@ class GridExample2 extends React.Component {
             categories: []
         };
         this.expandChange = this.expandChange.bind(this);
+        this.CommandCell = MyCommandCell;
+        this.DropDownCell = MyDropDownCell;
     }
 
     expandChange(event) {
@@ -59,9 +101,15 @@ class GridExample2 extends React.Component {
                     expandField="expanded"
                     expandChange={this.expandChange}
                 >
-                    <Column field="categoryId" title="ID" width="50px" />
-                    <Column field="categoryName" width="200px" title="Category Name" />
+                    <Column field="categoryId" title="ID"/>
+                    <Column field="categoryName" title="Category Name" />
                     <Column field="description" />
+                    <Column cell={this.CommandCell}/>
+                    <Column cell={this.DropDownCell} />
+                    <Column cell={this.DropDownCell} />
+                    <Column cell={this.DropDownCell} />
+                    <Column cell={this.DropDownCell} />
+                    <Column cell={this.DropDownCell} />
                 </Grid>
             </div>
         );
